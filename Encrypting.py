e831dc853858f2b082996ed3489dc7821dd064d3
RandomVapeUser <121972940+RandomVapeUser@users.noreply.github.com>
2023-11-28 21:50:45 +0000

    Basic

--- Encrypting.py
+++ Encrypting.py
@@ -4,9 +4,30 @@ import sys
 import os
 import time
 
-print("""
+while True:
 
+    print("""
 
+        [1] - Encrypt your files
+        [2] - Decrypt your files
+        [3] - Help
 
+        """)
+    
+    try:
+        X = int(input(" "))
+        break
+    except SyntaxError:
+        print("You need to enter a numbers not a letter you dummy")
+        time.sleep(2)
+        os.system("cls")
+        continue
 
-    """)
+def choice():
+
+    if X == 1:
+        encrypting()
+    elif X == 2:
+        decrypting()
+    elif X == 3:
+        help0()
